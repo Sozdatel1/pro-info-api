c80374fef2bea532f95bf4599d0e300d057a6747
Sozdatel1 <chess-pocht@ya.ru>
2026-01-26 16:18:06 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+app.use(cors()); // Чтобы твой сайт на GitHub мог достучаться до сервера
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: { origin: "*" } // Разрешаем подключения отовсюду
+});
+
+let onlineCount = 0;
+
+io.on('connection', (socket) => {
+    onlineCount++;
+    io.emit('updateCount', onlineCount); // Отправляем всем новое число
+
+    socket.on('disconnect', () => {
+        onlineCount--;
+        io.emit('updateCount', onlineCount);
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log('Счетчик работает!'));
