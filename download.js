30e1d34976350c1d18caf36ce361ddcb73a2e122
Sozdatel1 <chess-pocht@ya.ru>
2026-01-30 21:42:09 +0300

    Delete download.js

--- download.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-const fs = require('fs');
-
-axios.get('https://yastat.net')
-    .then(res => {
-        fs.writeFileSync('original_tag.js', res.data);
-        console.log("✅ Файл успешно скачан на сервер!");
-    })
-    .catch(err => console.log("❌ Ошибка: " + err.message));
