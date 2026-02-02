5717ffa5906e18a8918c793bbceb7924e171736f
Sozdatel1 <chess-pocht@ya.ru>
2026-02-02 16:22:51 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const axios = require('axios');
 const app = express();
 const { Hercai } = require('hercai'); 
-const herc = new Hercai(); 
+const herc = new Hercai({}); // Добавь скобки внутри!
 app.use(express.json());
 const { Redis } = require('@upstash/redis');
 const DEV_KEY = process.env.DEV_KEY; 
