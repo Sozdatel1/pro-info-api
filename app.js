7e5756a4a111e46a9ef7698d0aa3bbf091753595
Sozdatel1 <chess-pocht@ya.ru>
2026-05-30 13:49:10 +0300

    Update app.js

--- app.js
+++ app.js
@@ -38,7 +38,8 @@ app.use(cors({
     methods: ['GET', 'POST', 'OPTIONS', 'PATCH', 'DELETE',],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // 2. Ответ для главной страницы
 app.get('/', (req, res) => {
